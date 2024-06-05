9481a40751bbba03b7bbdd68c1f71a277aec2852
JoAraEsp <213345@ids.upchiapas.edu.mx>
2024-06-05 15:54:33 -0600

    changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/welcome', (req, res) => {
-    res.send('¡Ya quedo!  hhhh');
+    res.send('Hola mundito');
 });
 
 app.listen(port, () => {
