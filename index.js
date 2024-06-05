0b2b6feaa285463801b16d569e71bdfa9107fa4b
JoAraEsp <213345@ids.upchiapas.edu.mx>
2024-06-05 16:11:19 -0600

    changestwo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/welcome', (req, res) => {
-    res.send('Hola mundito');
+    res.send('Feliz navidad inmundo animal');
 });
 
 app.listen(port, () => {
